9bc40040e45002310825fb2e51259b5682935137
rositab91 <123000227+rositab91@users.noreply.github.com>
2023-07-05 16:53:31 +0200

    Add files via upload

---
+++ src/Exercise1/FirstThread.java
@@ -0,0 +1,24 @@
+package Exercise1;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FirstThread extends Thread {
+	private Logger log = LoggerFactory.getLogger(FirstThread.class);
+
+	public FirstThread() {
+	}
+
+	@Override
+	public void run() {
+		for (int i = 0; i < 10; i++) {
+			try {
+				log.info("*");
+				Thread.sleep(1000);
+			} catch (Exception e) {
+				log.error(e.getMessage());
+				e.printStackTrace();
+			}
+		}
+	}
+}
---
+++ src/Exercise1/Main.java
@@ -0,0 +1,27 @@
+package Exercise1;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Logger log = LoggerFactory.getLogger(Main.class);
+
+		FirstThread t1 = new FirstThread();
+
+//posso assegnare un nome a un thread
+		t1.setName("Thread Alfa");
+
+		SecondThread t2 = new SecondThread();
+		t2.setName("Thread Beta");
+
+		// non devo chiamare il metodo .run, altrimenti saro' sul main thread
+		// se chiamo .start viene creato un nuovo thread che chiamera' il metodo run
+		t1.start();
+		t2.start();
+
+	}
+
+}
